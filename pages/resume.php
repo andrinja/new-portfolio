e9a7d95362b81d5a97a103d45af613832ece1d6b
andrinja <andra.zeberga@gmail.com>
2017-11-07 15:36:44 +0100

    added nav to resume page

---
+++ pages/resume.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <?php include($_SERVER['DOCUMENT_ROOT'].'/includes/styles.php'); ?>
+</head>
+<body class="resume-content">
+<div class="blur-background"></div>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/includes/navigation.php');?>
+    <a class="back-btn" href="../index.php"><i class=" material-icons">arrow_back</i></a>
+    <img class="cv-image" src="../storage/cv.png" alt="">
+
+
+
+<?php include($_SERVER['DOCUMENT_ROOT'].'/includes/script.php'); ?>
+</body>
+</html>
