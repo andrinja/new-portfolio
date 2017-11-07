a4d237bbf926a30e2a215723eda6d44928f32897
andrinja <andra.zeberga@gmail.com>
2017-11-07 15:36:31 +0100

    links added to nav

--- includes/navigation.php
+++ includes/navigation.php
@@ -2,9 +2,11 @@
     <div class="nav-items">
         <a class="nav-item logo" href="">AZ</a>
         <div class="nav-item title-items">
+            <a class="title-item" href="">Home</a>
             <a class="title-item" href="">Digital projects</a>
-            <a class="title-item" href="../pages/art.php">Art</a>
-            <a class="title-item" href="">About</a>
+            <a class="title-item" href="../pages/art.php">Traditional art</a>
+            <a class="title-item" href="../pages/resume.php">Resume</a>
+            <a class="title-item" href="../pages/about.php">About me</a>
         </div>
         <div class="nav-item menu-icon" id="menu">
             <div class="top-line line"></div>
