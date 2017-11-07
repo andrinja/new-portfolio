a4cdc9cdea666d3d83d57fb0cfcbf7c0fd9f7eb4
andrinja <andra.zeberga@gmail.com>
2017-11-07 11:27:11 +0100

    no message

--- app.js
+++ app.js
@@ -1,46 +1,3 @@
-$(document).ready(function() {
-    $('#fullpage').fullpage();
-    console.log('full page initialized');
-});
-//  menu icon
-  $('.menu-icon').on('click', function(){
- // checks if menu is toggled or not
- if ($(this).hasClass('active')){
- // hides menu/switches button to 3 lines
- $('.menu-icon').removeClass('active');
- var tl = new TimelineMax();
- tl.to(".top-line", 1, {rotation:0, ease:Power4.easeOut,	y:0})
- // middle and bottom shows at the same time as top line -=1
- .to(".middle-line", 1, {x:0, opacity:1, ease:Power4.easeOut}, "-=1")
- .to(".bottom-line", 1, {rotation:0, ease:Power4.easeOut, y:0}, "-=1")
- }
- else {
- // shows menu/switches button to a cross
- $('.menu-icon').addClass('active');
- var tl = new TimelineMax();
- tl.to(".top-line", 1, {rotation:-45, ease:Power4.easeOut, y:7})
- .to(".middle-line", 1, {x:150, opacity:0, ease:Power1.easeOut}, "-=1")
- .to(".bottom-line", 1, {rotation:45, ease:Power4.easeOut, y:-7}, "-=1")
- }
- });
-
-
-
-$('#menu').on('click', function () {
-    if($('.title-items').hasClass('open')) {
-        $('.title-items').removeClass('open');
-        TweenMax.staggerTo(".title-items", 0.5, {scale:0.5, opacity:0, x:70, ease:Power0.easeOut}, 0.1);
-    }
-    else {
-        $('.title-items').addClass('open');
-        TweenMax.staggerTo(".title-items", 0, {scale:0.5, opacity:0, x:50, ease:Power0.easeOut});
-        TweenMax.staggerTo(".title-items", 1, {scale:1, opacity:1, x:0, delay:0.5, ease:Elastic.easeOut}, 0.2);
-    }
-
-});
-
-/*art.js*/
-
 
 function removeFilter(event) {
     var removeButton = event.target;
@@ -390,4 +347,44 @@ $('.buy-button').on('click', function (event) {
             $(document).off('click.closeBuyPage'); //click only once
         }
     })
+});
+$(document).ready(function() {
+    $('#fullpage').fullpage();
+    console.log('full page initialized');
+});
+//  menu icon
+  $('.menu-icon').on('click', function(){
+ // checks if menu is toggled or not
+ if ($(this).hasClass('active')){
+ // hides menu/switches button to 3 lines
+ $('.menu-icon').removeClass('active');
+ var tl = new TimelineMax();
+ tl.to(".top-line", 1, {rotation:0, ease:Power4.easeOut,	y:0})
+ // middle and bottom shows at the same time as top line -=1
+ .to(".middle-line", 1, {x:0, opacity:1, ease:Power4.easeOut}, "-=1")
+ .to(".bottom-line", 1, {rotation:0, ease:Power4.easeOut, y:0}, "-=1")
+ }
+ else {
+ // shows menu/switches button to a cross
+ $('.menu-icon').addClass('active');
+ var tl = new TimelineMax();
+ tl.to(".top-line", 1, {rotation:-45, ease:Power4.easeOut, y:7})
+ .to(".middle-line", 1, {x:150, opacity:0, ease:Power1.easeOut}, "-=1")
+ .to(".bottom-line", 1, {rotation:45, ease:Power4.easeOut, y:-7}, "-=1")
+ }
+ });
+
+
+
+$('#menu').on('click', function () {
+    if($('.title-items').hasClass('open')) {
+        $('.title-items').removeClass('open');
+        TweenMax.staggerTo(".title-items", 0.5, {scale:0.5, opacity:0, x:70, ease:Power0.easeOut}, 0.1);
+    }
+    else {
+        $('.title-items').addClass('open');
+        TweenMax.staggerTo(".title-items", 0, {scale:0.5, opacity:0, x:50, ease:Power0.easeOut});
+        TweenMax.staggerTo(".title-items", 1, {scale:1, opacity:1, x:0, delay:0.5, ease:Elastic.easeOut}, 0.2);
+    }
+
 });
