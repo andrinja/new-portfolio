8ca5147db45b7cecc451a4f02cbb4bf9c7a21cdf
andrinja <andra.zeberga@gmail.com>
2017-11-09 13:06:22 +0100

    no message

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 $('.item').on('click', function () {
     $('.item').removeClass('active');
         $(this).addClass('active');
+    var ButtonClass = $('.item.active').attr('class');
+    var ProgressBarClass = $('.line');
+    console.log(ButtonClass);
 });
+
+
+
+
 
 function removeFilter(event) {
     var removeButton = event.target;
--- dev/js/about.js
+++ dev/js/about.js
@@ -1,4 +1,10 @@
 $('.item').on('click', function () {
     $('.item').removeClass('active');
         $(this).addClass('active');
+    var ButtonClass = $('.item.active').attr('class');
+    var ProgressBarClass = $('.line');
+    console.log(ButtonClass);
 });
+
+
+
