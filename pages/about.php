2596637d61a7e84012b90813551ab842e78fefc2
andrinja <andra.zeberga@gmail.com>
2017-11-09 19:21:37 +0100

    progress bar and button style

--- pages/about.php
+++ pages/about.php
@@ -40,107 +40,55 @@
             </div>
             <div class="skill-items">
                 <div class="row">
-                    <div class="item">
+                    <div class="item html">
                         <div class="title">HTML</div>
                     </div>
-                    <div class="item">
+                    <div class="item javascript">
                         <div class="title">Javascript</div>
                     </div>
-                    <div class="item">
+                    <div class="item css">
                         <div class="title">CSSS3</div>
                     </div>
-                    <div class="item">
+                    <div class="item jquery">
                         <div class="title">Jquery</div>
                     </div>
-                    <div class="item">
+                    <div class="item php">
                         <div class="title">PHP</div>
                     </div>
-                    <div class="item">
+                    <div class="item mysql">
                         <div class="title">MySql</div>
                     </div>
-                    <div class="item">
+                    <div class="item sass">
                         <div class="title">SASS</div>
                     </div>
-                    <div class="item">
-                        <div class="title">Photoshop</div>
-                    </div>
-                    <div class="item">
-                        <div class="title">Illustrator</div>
-                    </div>
-                    <div class="item">
-                        <div class="title">Premiere Pro</div>
-                    </div>
-                    <div class="item">
-                        <div class="title">After effects</div>
-                    </div>
-                    <div class="item">
-                        <div class="title">Figma & Sketch</div>
-                    </div>
-                    <div class="item">
-                        <div class="title">SEO Optimization</div>
-                    </div>
-                    <div class="item">
-                        <div class="title">Wordpress</div>
-                    </div>
-                    <div class="item">
-                        <div class="title">Angular</div>
-                    </div>
-                    <div class="item">
-                        <div class="title">Premiere Pro</div>
-                    </div>
-                    <div class="item">
-                        <div class="title">Laracasts</div>
-                    </div>
-                    <div class="item">
-                    <div data-name="" class="item html">
-                        <div class="title">HTML</div>
-                    </div>
-                    <div data-name="" class="item javascript">
-                        <div class="title">Javascript</div>
-                    </div>
-                    <div data-name="" class="item css">
-                        <div class="title">CSSS3</div>
-                    </div>
-                    <div data-name="" class="item jquery">
-                        <div class="title">Jquery</div>
-                    </div>
-                    <div data-name="" class="item php">
-                        <div class="title">PHP</div>
-                    </div>
-                    <div data-name="" class="item mysql">
-                        <div class="title">MySql</div>
-                    </div>
-                    <div data-name="" class="item sass">
-                        <div class="title">SASS</div>
-                    </div>
-                    <div data-name="" class="item photoshop">
+                    <div class="item photoshop">
                         <div class="title eight">Photoshop</div>
                     </div>
-                    <div data-name="" class="item illustrator">
+                    <div class="item illustrator">
                         <div class="title">Illustrator</div>
                     </div>
-                    <div data-name="" class="item premiere">
+                    <div class="item premiere">
                         <div class="title">Premiere Pro</div>
                     </div>
-                    <div data-name="" class="item after-effects">
+                    <div class="item after-effects">
                         <div class="title">After effects</div>
                     </div>
-                    <div data-name="" class="item figma">
+                    <div class="item figma">
                         <div class="title">Figma & Sketch</div>
                     </div>
-                    <div data-name="" class="item seo">
+                    <div class="item seo">
                         <div class="title">SEO Optimization</div>
                     </div>
-                    <div data-name="" class="item wordpress">
+                    <div class="item wordpress">
                         <div class="title">Wordpress</div>
                     </div>
-                    <div data-name="" class="item angular">
+                    <div class="item angular">
                         <div class="title">Angular</div>
                     </div>
-                    <div data-name="" class="item laracasts">
+                    <div class="item laracasts">
                         <div class="title">Laracasts</div>
                     </div>
-                    <div data-name="" class="item bootstrap">
+                    <div class="item bootstrap">
                         <div class="title">Bootrstrap</div>
                     </div>
                 </div>
